5720ade98426847e0ea768c914e2bb482f15d9f9
Roman <65686226+morozit@users.noreply.github.com>
2020-11-21 01:52:22 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import './SASS/main.scss';
 
-
-import { startBtnClickHandler, stopBtnClickHandler } from './js/start-stop';
-
-
-const startBtn = document.querySelector('[data-action="start"]');
-const stopBtn = document.querySelector('[data-action="stop"]');
-
-startBtn.addEventListener('click', startBtnClickHandler);
-stopBtn.addEventListener('click', stopBtnClickHandler);
+import './js/main.js';
