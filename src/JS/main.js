8006e86e881d186b3bc66627f34826743b64bc4f
Roman <65686226+morozit@users.noreply.github.com>
2020-11-22 18:26:03 +0200

    update

--- src/JS/main.js
+++
@@ -1,3 +0,0 @@
-import './js/task-1';
-import './js/task-2';
-import './js/task-3';
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './SASS/main.scss';
 
-import './js/main.js';
+import './js/task-1';
+import './js/task-2';
+import './js/task-3';
